59d410ebd97dc35bdd008f0cedd4856b69933863
Ingo Ruhnke <grumbel@gmail.com>
2022-08-19 16:50:16 +0200

    Initial checkin

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    tinycmmc.url = "github:grumbel/tinycmmc";
+    tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
+    tinycmmc.inputs.flake-utils.follows = "flake-utils";
+
+    openal-soft_src.url = "github:kcat/openal-soft?ref=1.22.2";
+    openal-soft_src.flake = false;
+  };
+
+  outputs = { self, nixpkgs, flake-utils, tinycmmc, openal-soft_src }:
+    tinycmmc.lib.eachWin32SystemWithPkgs (pkgs:
+      {
+        packages = rec {
+          default = openal-soft;
+
+          openal-soft = pkgs.stdenv.mkDerivation {
+            pname = "openal-soft";
+            version = "1.22.2";
+
+            src = openal-soft_src;
+
+            nativeBuildInputs = [
+              pkgs.buildPackages.cmake
+              # pkgs.cmake
+            ];
+          };
+        };
+      }
+    );
+}
