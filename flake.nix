856ddfa368e3b4a22027dbb1bf8a4d8c3264e5d3
Ingo Ruhnke <grumbel@gmail.com>
2022-08-19 17:36:21 +0200

    Remove flake-utils dependency

--- flake.nix
+++ flake.nix
@@ -1,17 +1,15 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-    flake-utils.url = "github:numtide/flake-utils";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
     tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
-    tinycmmc.inputs.flake-utils.follows = "flake-utils";
 
     openal-soft_src.url = "github:kcat/openal-soft?ref=1.22.2";
     openal-soft_src.flake = false;
   };
 
-  outputs = { self, nixpkgs, flake-utils, tinycmmc, openal-soft_src }:
+  outputs = { self, nixpkgs, tinycmmc, openal-soft_src }:
     tinycmmc.lib.eachWin32SystemWithPkgs (pkgs:
       {
         packages = rec {
